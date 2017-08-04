afc1e6a9e95c826401edcae292df72560acbacf5
MashaLypa <MashaLypa@yandex.ua>
2017-08-05 00:13:38 +0300

    part 1. Simple bot

--- Program.cs
+++ Program.cs
@@ -2,13 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace chat_bot_vkAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        // удаление букв
+        static string Trim(string str, char[] chars)
         {
+            string strA = str; // копирование строки (не правильное)
+
+            // удаление символов
+            for (int i = 0; i < chars.Length; i++)
+            {
+                strA = strA.Replace(char.ToString(chars[i]), "");
+            }
+
+            return strA;
         }
+
+        // генерация ответа
+        static string Ans(string q)
+        {
+            string tr = "():^=!?", // символы, которые надо удалять
+            ans = ""; // ответ бота
+
+            q = q.ToLower(); // перевод в нижний регистр
+            q = Trim(q, tr.ToCharArray()); // удаление букв
+            string[] BaseAnswer = File.ReadAllLines("BaseAnswer.txt"); // загрузка файла
+
+            for (int i = 0; i < BaseAnswer.Length; i += 2)
+            {
+                if (q == BaseAnswer[i])
+                {
+                    ans = BaseAnswer[i + 1]; // выдает ответ
+                    break; // завершает цикл
+                }
+            }
+
+            return ans; // ответ
+        }
+
+        public static void Main(string[] args)
+        {
+            // бесконечный цикл
+            while (true)
+            {
+                Console.Write("Your answer: ");
+                string q = Console.ReadLine(); // ввод вопроса
+                Console.WriteLine("Bot say: " + Ans(q) + "\n"); // вывод ответа
+            }
+        }
+
     }
 }
